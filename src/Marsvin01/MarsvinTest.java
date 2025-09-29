9cea933163cfc06f53c5bf0e12b54ff7a1e8b6e2
MagnusNH <Magn763j@stud.ek.dk>
2025-09-29 15:33:24 +0200

    Ss

--- src/Marsvin01/MarsvinTest.java
+++ src/Marsvin01/MarsvinTest.java
@@ -1,7 +1,43 @@
 package Marsvin01;
 
+import java.io.*;
+
 public class MarsvinTest {
-    public static void main (String[] args){
-        ArrayList<Marsvins> marsvin = new ArrayList<>();
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\magnu\\Desktop\\Marsvin.txt");
+
+        //Counting number of lines to put in array.
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while (br.readLine() != null) {
+                count++;
+            }
+        } catch (IOException e) {
+            System.out.println("Fejl ved læsning: " + e.getMessage());
+        }
+
+        Marsvins[] marsvins = new Marsvins[count];
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(";");
+                if (parts.length == 3) {
+                    String name = parts[0];
+                    String race = parts[1];
+                    int food = Integer.parseInt(parts[2]);
+                    marsvins[i] = new Marsvins(name, race, food);
+                    i++;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
+
--- src/Marsvin01/Marsvins.java
+++ src/Marsvin01/Marsvins.java
@@ -1,4 +1,25 @@
 package Marsvin01;
 
 public class Marsvins {
+    private String name;
+    private String race;
+    private int food;
+
+    public Marsvins (String name, String race, int food){
+        this.name = name;
+        this.race = race;
+        this.food = food;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getRace(){
+        return race;
+    }
+
+    public int getFood(){
+        return food;
+    }
 }
